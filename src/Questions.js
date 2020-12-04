8d6fe0d1ffe6d92d45fcc3171a1f26660edbc7ec
hugogit120 <hugomercalibre@gmail.com>
2020-12-04 15:19:22 +0100

    firt commit, the question box model design creation in progress

---
+++ src/Questions.js
@@ -0,0 +1,42 @@
+export const quizData = [
+    {
+        question: "What is meant by HTML?",
+        a: "Hiper text markup language",
+        b: "Hindsight tautology maintainable language",
+        c: "Helicopter taboo monstrocity lagoon",
+        d: "Hello this means latency",
+        correct: "a"
+    },
+    {
+        question: "Who created the world first web site?",
+        a: "Bill Gates",
+        b: "Neil Armstrong",
+        c: "Tim Berners-Lee",
+        d: "Steve Jobs",
+        correct: "c"
+    },
+    {
+        question: "What year was Javascript launched?",
+        a: "1940",
+        b: "1995",
+        c: "2004",
+        d: "None of the above",
+        correct: "b"
+    },
+    {
+        question: "What is the most used programming language in 2020",
+        a: "Java",
+        b: "Javascript",
+        c: "Python",
+        d: "PHP",
+        correct: "c"
+    },
+    {
+        question: "what is CSS used for",
+        a: "Define the style and design of a webpage",
+        b: "Facilitates and helps when writing other languages",
+        c: "Create functions that helps managing applications",
+        d: "For testing UI components",
+        correct: "a"
+    }
+]
---
+++ src/components/QuestionBox.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+const QuestionBox = (props) => {
+
+
+
+
+}
+
+export default QuestionBox
